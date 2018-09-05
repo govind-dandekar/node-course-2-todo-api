74fe1e3dd4c0bf684519842ef09536821a22034b
Govind Dandekar <govind@Govinds-MacBook-Air.local>
2018-09-05 16:17:35 -0700

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} =require('mongodb');
+const _ = require('lodash');
 
 
 var {mongoose} = require('./db/mongoose.js');
@@ -44,9 +45,7 @@ app.get('/todos', (req, res) => {
   });
 })
 
-// GET /todos/id
-// need to make id part dynamic --
-//create id var on request object by appending :id
+
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -63,12 +62,9 @@ app.get('/todos/:id', (req, res) => {
       res.status(400).send();
     })
   }
-
-  //res.send(req.params);
 });
 
 app.delete('/todos/:id', (req, res) => {
-  // get the ID -- pull of the id
   var id = req.params.id;
 
 if (!ObjectID.isValid(id)){
@@ -84,6 +80,41 @@ if (!ObjectID.isValid(id)){
     res.status(400).send();
   })
 }
+})
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  //updates will be stored within the body
+  //user can send along properties that aren't part of object or we don't want to update (completedAt)
+
+  //created body var with subset of things
+  var body = _.pick(req.body, ['text', 'completed']) //users can only updated two properties
+
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  //set completedAt
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime(); //returns JS timestamp -- MS from Jan 1 1970
+  } else {
+    //when update makes the boolean false
+    body.completed = false;
+    body.completedAt = null; //when
+  }
+  //make query to update DB
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+  //checking completed for true -- set completedAt
+
+
 
 })
 
