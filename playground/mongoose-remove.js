111e49be0afe08f401bf18f2b97625beaca1be8f
Govind Dandekar <govind@Govinds-MacBook-Air.local>
2018-09-04 22:02:09 -0700

    Added DELETE /todos/:id route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -3,19 +3,7 @@ const {Todo} = require('./../server/models/todo');
 const {ObjectID} =require('mongodb');
 const {User} = require('./../server/models/user')
 
-//mongoose deletion methods
-// Todo.remove({}) -- delete all that match
 
-// Todo.remove({}).then((res)=>{
-//   console.log(res);
-// }) // do not get docs back
-
-// Todo.findOneAndRemove({}).then((res)=> {
-//   //gets removed docs back
-//
-// })
-
-// Todo.findOneAndRemove({'_id: '})
 
 
 Todo.findByIdAndRemove('5b8f5a70fc0fd652f649e9a7').then((todo) => {
